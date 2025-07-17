fa497cc763dc399516c2037683421005e700a3cb
karimdaouk3 <karimdaouk3@gmail.com>
2025-07-17 18:01:47 -0400

    Alert: Put quotes around word in 'added to KLA term bank' message

--- static/js/editor.js
+++ static/js/editor.js
@@ -444,7 +444,7 @@ class LanguageToolEditor {
                 this.saveTerm(text);
                 this.ignoreCurrentSuggestion();
                 this.hidePopup();
-                this.showStatus(`${text} added to KLA term bank`, 'success');
+                this.showStatus(`"${text}" added to KLA term bank`, 'success');
             };
         } else if (blueBtn.parentElement) {
             blueBtn.parentElement.removeChild(blueBtn);
